30bc823b3f93d3d2a07342942b4108f7e4b91d96
Marcelismus <Marcelismus@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-01-02 15:30:00 +0000

    Version 1.0.1
    git-svn-id: https://plugins.svn.wordpress.org/justin-feeder/tags/1.0.1@647016 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ justin-feeder.php
@@ -0,0 +1,44 @@
+<?php
+/*
+Plugin Name: JustIn Feeder
+Plugin URI: http://www.blogtogo.de/
+Description: Via Shortcode Content in einen Artikel oder eine Seite einfügen, der nur wahlweise nur im Feed oder auch nicht im Feed angezeigt wird.
+Author: Marcel Schmilgeit
+Version: 1.0.0
+Author URI: http://www.blogtogo.de
+*/
+
+define("JUSTINFEEDER_PLUGIN_NAME", 'JustIn Feeder'); 
+define("JUSTINFEEDER_PLUGIN_VERSION", '1.0.0');
+define("PLUGIN_BASE_DIR", basename(dirname(dirname(__FILE__))));		
+
+
+function JustInFeeder_add_buttons($buttons) {
+	array_push($buttons, "separator", "JustinFeeder_Button");
+	return $buttons;
+}
+
+
+function JustInFeeder_register($plugin_array) {
+	$plugin_array["justinfeeder"] = plugins_url('btn.js', __FILE__);
+	return $plugin_array;
+}
+
+
+function JustInFeeder_shortcode_infeed($atts, $content = null) {
+	if(is_feed()) { return $content; }
+}
+
+
+function JustInFeeder_shortcode_notinfeed($atts, $content = null) {
+	if(!is_feed()) { return $content; }
+}
+
+
+add_shortcode('infeed', 'JustInFeeder_shortcode_infeed');
+add_shortcode('notinfeed', 'JustInFeeder_shortcode_notinfeed');
+ 
+add_filter('mce_external_plugins', "JustInFeeder_register");
+add_filter('mce_buttons', 'JustInFeeder_add_buttons', 0);
+
+?>
